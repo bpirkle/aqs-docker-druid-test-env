63ab8c5ffdadd2fdfbef8c943777ecdb2c861445
Santiago Faci <sfaci@wikimedia.org>
2023-06-21 18:45:55 +0200

    Removed (aparently) useless code that waits for ingestion to finish

--- aqs-docker-druid-test-env-build/ingest.sh
+++ aqs-docker-druid-test-env-build/ingest.sh
@@ -3,32 +3,11 @@
 # If this is never successful, this waits forever.
 # A max timeout with an error message might be preferable.
 wait_for_datasource() {
-    # echo "Task status:"
-    # curl -s "http://localhost:88888/druid/indexer/v1/task/$task/status"
-
     datasources="$(curl -s http://localhost:8888/druid/v2/datasources)"
-
-    # echo "Loaded datasources: $datasources"
 
     if [ "$datasources" != '["aqs_test"]' ]; then
         sleep 10
         wait_for_datasource
-    fi
-}
-
-# If this is never successful, this waits forever.
-# A max timeout with an error message might be preferable.
-wait_for_all_rows() {
-    # query the row count from the database, then extract the numerical portion of the json result
-    QUERY='{"query":"SELECT count(*) AS \"count\" FROM \"aqs_test\""}'
-    count=$(curl -s -X POST -H 'Content-Type: application/json' --data "$QUERY" http://localhost:8888/druid/v2/sql)
-    count=${count//[!0-9]/}
-
-    echo "Rows loaded: $count"
-    if [ $count < $1 ]; then
-        echo -ne "$count of $1 rows ingested"
-        sleep 10
-        wait_for_all_rows
     fi
 }
 
@@ -64,6 +43,3 @@ curl -s -X POST \
 echo -e "\nWaiting for ingestion to finish. Please be patient, this may take up a while..."
 echo "Waiting for datasource to be available . . ."
 wait_for_datasource
-echo "Datasource loaded, loading rows"
-total=$(sed -n '$=' /data/test-data.csv)
-wait_for_all_rows $total
